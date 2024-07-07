39f488492c9e7cc885609318ccaf57475a41ff96
anonymous-72 <sergey.bordeniuc1@gmail.com>
2024-07-07 21:30:22 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { MenuPageComponent } from "./pages/menu-page/menu-page.component";
+import {ReservationComponent} from "./pages/reservation/reservation.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Noir Nosh'
+  },
+  {
+    path: 'menu',
+    component: MenuPageComponent,
+    title: 'Menu'
+  },
+  {
+    path: 'reservation',
+    component: ReservationComponent,
+    title: 'Reservation'
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from "./pages/home/components/hero/hero.component"
+import { NavLinkComponent } from './components/nav-link/nav-link.component';
+import { ButtonDarkComponent } from './components/button-dark/button-dark.component';
+import { ButtonLightComponent } from './components/button-light/button-light.component';
+import { AboutComponent } from "./pages/home/components/about/about.component";
+import { MenuComponent } from "./pages/home/components/menu/menu.component";
+import { ReviewComponent } from "./pages/home/components/review/review.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { FooterLinkComponent } from './components/footer-link/footer-link.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuPageComponent } from './pages/menu-page/menu-page.component';
+import { TitleComponent } from './pages/menu-page/title/title.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavLinkComponent,
+    HeroComponent,
+    ButtonDarkComponent,
+    ButtonLightComponent,
+    AboutComponent,
+    MenuComponent,
+    ReviewComponent,
+    FooterComponent,
+    FooterLinkComponent,
+    HomeComponent,
+    MenuPageComponent,
+    TitleComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
